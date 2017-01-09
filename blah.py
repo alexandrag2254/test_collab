9c76ff0f3b95edcbea2462e99cfd2b4ca08af4f9
Nathan Dataguake Basanese <v6@users.noreply.github.com>
2017-01-09 13:45:33 -0800

    Create blah.py

---
+++ blah.py
@@ -0,0 +1,15 @@
+/usr/bin/env python3
+  ##  This program's expected output is "My name is Nathan", but replace Nathan with whatever the person's name is.
+  ##  Unfortunately, the output is "My name is what".
+  ##  This is a pretty minimal, shoddy program.
+  ##  Is this a 'fatal' error, or a 'logic' error?
+  ##  What's broken here?
+
+name = 'what'
+
+print ('My name is ' + name)
+
+  ##  Here's where I'd put my name:
+name = 'Nathan'
+
+  ##  Hint: fixable by reordering some lines.
