361ad38702442d85005b49c9141590ca539599a8
Nathan Dataguake Basanese <v6@users.noreply.github.com>
2017-02-05 15:10:45 -0800

    // , Introduce another conflict
      // , This time I have edited every line

--- blah.py
+++ blah.py
@@ -1,11 +1,7 @@
-/usr/bin/env python3
-  ##  This program's expected output is "My name is Nathan", but replace Nathan with whatever the person's name is.
-  ##  Unfortunately, the output is "My name is what".
-  ##  This is a pretty minimal, shoddy program.
-  ##  Is this a 'fatal' error, or a 'logic' error?
-  ##  What's broken here?
+/usr/bin/env python
+  ##  This program's expected output is "My name is Nathan", but replace Nathan with whatever the person's name is....
 
-name = 'Alex'
+name = 'Alexandra G'
 print ('My name is ' + name and I am a terrible Git developer)
 
 ## Michael is typing a comment
