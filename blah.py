95ca01e6a809201d2b8f5f4d6bb21b89281b7f45
Michael Folkson <michael@riskbazaar.org>
2017-03-02 13:38:38 -0800

    correcting Python code

--- blah.py
+++ blah.py
@@ -2,6 +2,6 @@
   ##  This program's expected output is "My name is Nathan", but replace Nathan with whatever the person's name is....
 
 name = 'Alexandra G'
-print ('My name is ' + name and I am a terrible Git developer)
+print (My name is +name and I am a terrible Git developer)
 
 ## Michael is typing a comment
